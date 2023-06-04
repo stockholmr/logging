677b05d2df10ce9be2a74b8fede95dbaf6438df9
skm <r.stockholm@protonmail.com>
2023-06-04 14:01:44 +0100

    updated log level

--- baselogger.go
+++ baselogger.go
@@ -18,42 +18,42 @@ func (b *baseLogger) llog(message *Message) {
 
 func (b *baseLogger) Error(v ...interface{}) {
 	if b.useCSVFormat {
-		b.llog(NewCSVMessage(LOGLEVEL_ERROR, v...))
+		b.llog(NewCSVMessage(ERROR, v...))
 		return
 	}
-	b.llog(NewMessage(LOGLEVEL_ERROR, fmt.Sprint(v...)))
+	b.llog(NewMessage(ERROR, fmt.Sprint(v...)))
 }
 
 func (b *baseLogger) Info(v ...interface{}) {
 	if b.useCSVFormat {
-		b.llog(NewCSVMessage(LOGLEVEL_INFO, v...))
+		b.llog(NewCSVMessage(INFO, v...))
 		return
 	}
-	b.llog(NewMessage(LOGLEVEL_INFO, fmt.Sprint(v...)))
+	b.llog(NewMessage(INFO, fmt.Sprint(v...)))
 }
 
 func (b *baseLogger) Debug(v ...interface{}) {
 	if b.useCSVFormat {
-		b.llog(NewCSVMessage(LOGLEVEL_DEBUG, v...))
+		b.llog(NewCSVMessage(DEBUG, v...))
 		return
 	}
-	b.llog(NewMessage(LOGLEVEL_DEBUG, fmt.Sprint(v...)))
+	b.llog(NewMessage(DEBUG, fmt.Sprint(v...)))
 }
 
 func (b *baseLogger) Request(v ...interface{}) {
 	if b.useCSVFormat {
-		b.llog(NewCSVMessage(LOGLEVEL_REQUEST, v...))
+		b.llog(NewCSVMessage(REQUEST, v...))
 		return
 	}
-	b.llog(NewMessage(LOGLEVEL_REQUEST, fmt.Sprint(v...)))
+	b.llog(NewMessage(REQUEST, fmt.Sprint(v...)))
 }
 
 func (b *baseLogger) Fatal(v ...interface{}) {
 	if b.useCSVFormat {
-		b.llog(NewCSVMessage(LOGLEVEL_FATAL, v...))
+		b.llog(NewCSVMessage(FATAL, v...))
 		os.Exit(1)
 		return
 	}
-	b.llog(NewMessage(LOGLEVEL_FATAL, fmt.Sprint(v...)))
+	b.llog(NewMessage(FATAL, fmt.Sprint(v...)))
 	os.Exit(1)
 }
